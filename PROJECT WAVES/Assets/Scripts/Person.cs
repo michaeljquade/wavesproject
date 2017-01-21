0387289c5539ae26f9a9e5495b7567bbca96e1af
EverettG <egwyn@purdue.edu>
2017-01-21 14:14:45 -0500

    Commiting the new prefabs
    Chandeliers are sexy

--- PROJECT WAVES/Assets/Scripts/Person.cs
+++ PROJECT WAVES/Assets/Scripts/Person.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Person : MonoBehaviour {
 	public enum state  {Approve,Indifferent,Against,Rage};
-
+	string[] peopleSay; //stuff people say Vishnu will add it all in
 	public state feeling;
+	float Textvisible;
+
 	// Use this for initialization
 	void Start () {
 	}
